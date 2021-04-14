3490d563dc4fda20de3cd221e2532623655e3ed2
Diego Ignacio Saavedra González <diego.saavedrag@mail.udp.cl>
2021-04-14 16:53:53 -0400

    modifiqué ruta

--- Backend/routes/alertas.js
+++ Backend/routes/alertas.js
@@ -8,10 +8,11 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
-const { creaAlerta , getAlertas } = require('../controllers/alertas');
+const { creaAlerta , getAlertas, getCurrentAlertas } = require('../controllers/alertas');
 
 router.post('/newAlerta', creaAlerta);
 router.get('/getAlertas', getAlertas);
+router.get('/getAlertasNow', getCurrentAlertas);
 
 module.exports = router;
 
