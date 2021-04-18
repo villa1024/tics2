781b87cba0dbdf54df064996e4596f207c95d535
Xuvisking <vicente.lopez@mail.udp.cl>
2021-04-18 18:20:50 -0400

    arreglando el login

--- frontend/src/layouts/Login/Login.js
+++ frontend/src/layouts/Login/Login.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import { useForm } from 'react-hook-form'
 
 // reactstrap components
 import {
@@ -16,16 +17,36 @@ import {
 } from "reactstrap";
 
 function Login() {
+
+  const {register, errors, handleSubmit} = useForm();
+
+  const [datos, setDatos] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleInputChange = (event) => {
+    setDatos({
+      ...datos,
+      [event.target.name] : event.target.value
+    })
+  }
+
+  const enviarDatos = (event) => {
+    event.preventDefault();
+    console.log(datos.username + ' ' + datos.password)
+  }
+
   return (
     <>
       <div className="content">
+      <Form onSubmit={enviarDatos}>
           <Col className="ml-auto mr-auto mt-5 text-center" md="4">
             <Card>
               <CardHeader>
                 <h2 className="title">Iniciar Sesión</h2>
               </CardHeader>
               <CardBody>
-                <Form>
                   <Row>
                     <Col className="ml-auto mr-auto text-center" md="10">
                       <FormGroup>
@@ -34,6 +55,8 @@ function Login() {
                           defaultValue=""
                           placeholder="Usuario"
                           type="text"
+                          name="username"
+                          onChange={handleInputChange}
                         />
                       </FormGroup>
                     </Col>
@@ -45,20 +68,22 @@ function Login() {
                         <Input
                           defaultValue=""
                           placeholder="Contraseña"
-                          type="text"
+                          type="password"
+                          name="password"
+                          onChange={handleInputChange}
                         />
                       </FormGroup>
                     </Col>
                   </Row>
-                </Form>
               </CardBody>
               <CardFooter>
-                <Button className="btn-fill" color="primary" type="submit">
+                <Button className="btn-primary" color="primary" type="submit">
                   Enviar
                 </Button>
               </CardFooter>
             </Card>
           </Col>
+          </Form>
       </div>
     </>
   );
